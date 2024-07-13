f1a649e88a87de8e2193ace5a822fbe18cd2dea9
jota <juan.erices04@inacapmail.cl>
2024-07-13 18:36:58 -0400

    link de fondo_login releases

--- login.py
+++ login.py
@@ -6,7 +6,7 @@ def main(page: ft.Page):
     page.title = "Login"
     
     # Agregar imagen de fondo
-    bg_image = ft.Image(src="https://github.com/j0t4inc0de/my_login_flet/blob/main/assets/fondo_login.png", fit=ft.ImageFit.COVER, width=page.window.width, height=page.window.height)
+    bg_image = ft.Image(src="https://private-user-images.githubusercontent.com/119741641/348506387-a70b684b-3d23-4c5d-a18b-c784bc880eb6.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MjA5MTA0NTMsIm5iZiI6MTcyMDkxMDE1MywicGF0aCI6Ii8xMTk3NDE2NDEvMzQ4NTA2Mzg3LWE3MGI2ODRiLTNkMjMtNGM1ZC1hMThiLWM3ODRiYzg4MGViNi5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwNzEzJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDcxM1QyMjM1NTNaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT04YWI3NGM4MWU2MzA2YzczNDdjOTE0OGY0NTNhMzcwMTk4MGRiZThjMGEwOTRjNDFhMmFiZjU1MmY2NzY4MDc3JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.K2gKwJBJliFelBO-jdlH98cXU2Dz0CgyHzEIDO-jPT4", fit=ft.ImageFit.COVER, width=page.window.width, height=page.window.height)
     page.add(bg_image)
 
     login_text = ft.Text("Login", color='black', size=40, weight=ft.FontWeight.BOLD)
