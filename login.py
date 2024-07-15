be20d97eb645e0e705f9fd37beb48fdec35c279b
jota <juan.erices04@inacapmail.cl>
2024-07-15 17:27:27 -0400

    funciones para botones

--- login.py
+++ login.py
@@ -1,22 +1,32 @@
 import flet as ft
 
+
 def main(page: ft.Page):
     page.window.width = 375
     page.window.height = 812
     page.title = "Login"
     # page.bgcolor = "#ffffff"
+    # Funciones
+    def iniciar_sesion(e):
+        print("\nSe apreto el boton 'Login'")
+    def registrar_usuario(e):
+        print("\nSe apreto el boton de registro")
+    def contrasena_olvidada(e):
+        print("\nSe apreto el boton de 'contraseña olvidada'")
     # Variables
-    
     # path_logo = "assets/icono.png"
     # path_fondo_login = "assets/fondo_login.png"
     # path_logo = "https://private-user-images.githubusercontent.com/119741641/348514845-f887ae47-b72c-4c27-89c6-50542c1b9007.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MjA5MzAzNTIsIm5iZiI6MTcyMDkzMDA1MiwicGF0aCI6Ii8xMTk3NDE2NDEvMzQ4NTE0ODQ1LWY4ODdhZTQ3LWI3MmMtNGMyNy04OWM2LTUwNTQyYzFiOTAwNy5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwNzE0JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDcxNFQwNDA3MzJaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT1iYmQzYTk2ZTk0MTkyMjg0ZGQ0NGQxNzAyZDE5Mzc2MjQ3NmM0NzJlODcxZGFmNTkzNjE3YWQ3ODZhYzMyZTY5JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.--nWqpqAVlK3CH0te9L3VKi0o99BRUy0uuRscDR-sTA"
     path_fondo_login = "https://github.com/j0t4inc0de/my_login_flet/releases/download/fondo/fondo_login.png"
     bg_image = ft.Image(src=path_fondo_login, fit=ft.ImageFit.COVER, width=page.window.width, height=page.window.height)
+    
     login_text = ft.Text("Login", color='black', size=40, weight=ft.FontWeight.BOLD)
     username = ft.TextField(label="Usuario", border_radius=15, width=300, filled=True, border_color="transparent", bgcolor="#8F8E8E")
     password = ft.TextField(label="Contraseña", password=True, border_radius=15, width=300, filled=True, border_color="transparent", bgcolor="#8F8E8E")
+    # Botones
     password_olvidada = ft.TextButton(
         text="¿Se te olvido la contraseña?",
+        on_click=contrasena_olvidada,
         style=ft.ButtonStyle(
         color="#000000",
         bgcolor="#ffffff",
@@ -25,6 +35,7 @@ def main(page: ft.Page):
     )
     register_btn = ft.TextButton(
         text="Registrate",
+        on_click=registrar_usuario,
         style=ft.ButtonStyle(
         color="#000000",
         bgcolor="#ffffff",
@@ -33,6 +44,7 @@ def main(page: ft.Page):
     )
     login_btn = ft.FilledButton(
         "Login",
+        on_click=iniciar_sesion,
         width=300,
         height=50,
         style=ft.ButtonStyle(
